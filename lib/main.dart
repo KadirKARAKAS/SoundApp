965336ef58217ac0152e2048fad9a099591adeb3
Kadir KARAKAS <kadirkarakas5546@gmail.com>
2024-05-31 17:10:51 +0300

    savess

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:soundapp/homePage.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(HomePage());
+  await Firebase.initializeApp();
+  runApp(const MaterialApp(
+    home: HomePage(),
+  ));
 }
